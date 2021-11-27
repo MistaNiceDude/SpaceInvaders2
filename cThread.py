7be138ec622faffb7c12f24799f9577eae7dbc4e
Desmondchaser27 <browndesmond27@gmail.com>
2021-11-27 01:04:01 -0500

    Just adding threads

---
+++ cThread.py
@@ -0,0 +1,15 @@
+## Custom thread subclass
+
+import threading
+
+class Threadable(threading.Thread):
+    def __init__(self, threadID: int, name: str, delay_time: int) -> None:
+        threading.Thread.__init__(self)
+        self.threadID: int = threadID
+        self.name: str = name
+        self.delay_time: int = delay_time
+        self.mutex: threading.Lock = threading.Lock()
+    
+    # Meant to be overridden
+    def run(self) -> None:
+        pass
